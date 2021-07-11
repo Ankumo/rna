525aa80d70c34e23937593bf3bb0714d60adf702
Ankumo <ankubear@gmail.com>
2021-07-11 23:45:22 +0800

    fix sourceFilter eslint and ts errors

--- packages/esbuild-plugin-html/lib/sourceFilter.js
+++ packages/esbuild-plugin-html/lib/sourceFilter.js
@@ -1,6 +1,5 @@
 /**
  * Filter path to not bundle external resources
+ * @param {string|undefined} path Path to the resource
  */
-export default path => {
-    return path && path.indexOf("://") < 0 && !path.startsWith("//");
-};
+export default path => path && path.indexOf('://') < 0 && !path.startsWith('//');
